5207bf3bf90c69c57427795031327f4a3988f271
congtrieu98 <trieunguyen2806@gmail.com>
2023-12-29 16:10:07 +0700

    change time cron job

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,11 +1,22 @@
+import { EmailTemplate } from "@/components/emails/FirstEmail";
+import { resend } from "@/lib/email/index";
+import { emailSchema } from "@/lib/email/utils";
+import { NextResponse } from "next/server";
+
 export async function GET() {
-    const result = await fetch(
-      'http://worldtimeapi.org/api/timezone/America/Chicago',
-      {
-        cache: 'no-store',
-      },
-    );
-    const data = await result.json();
-   
-    return Response.json({ datetime: data.datetime });
+  const email = 'trieunguyen2806@gmail.com'
+  const firstName = 'cong trieu suzu'
+  try {
+    const data = await resend.emails.send({
+      from: `SZG <${process.env.RESEND_EMAIL}>`,
+      to: [email],
+      subject: "Hello world!",
+      react: EmailTemplate({ firstName: firstName }),
+      text: "Email powered by Resend creon job.",
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
   }
+}
