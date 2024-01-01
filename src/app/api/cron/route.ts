122f1aa1781d01b2cf5b7371c62bfd81c2c03541
congtrieu98 <trieunguyen2806@gmail.com>
2024-01-01 21:33:09 +0700

    fix code

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,36 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client'
 import { EmailTemplate } from "@/components/emails/FirstEmail";
-import { getUserAuth } from "@/lib/auth/utils";
-import { CompleteTask } from "@/lib/db/schema/tasks";
 import { resend } from "@/lib/email/index";
-import { emailSchema } from "@/lib/email/utils";
 import { trpc } from "@/lib/trpc/client";
 import { NextResponse } from "next/server";
+import { useEffect } from "react";
 
-// export async function GET() {
-//   const email = 'trieunguyen2806@gmail.com'
-//   const firstName = 'cong trieu suzu'
-//   try {
-//     const data = await resend.emails.send({
-//       from: `SZG <${process.env.RESEND_EMAIL}>`,
-//       to: [email],
-//       subject: "Hello world Cron Job!",
-//       react: EmailTemplate({ firstName: firstName }),
-//       text: "Email powered by Resend creon job.",
-//     });
+export async function GET() {
+  const email = 'trieunguyen2806@gmail.com'
+  const firstName = 'cong trieu suzu'
+  try {
+    const data = await resend.emails.send({
+      from: `SZG <${process.env.RESEND_EMAIL}>`,
+      to: [email],
+      subject: "Hello world Cron Job!",
+      react: EmailTemplate({ firstName: firstName }),
+      text: "Email powered by Resend creon job.",
+    });
 
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     return NextResponse.json({ error });
-//   }
-// }
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
 
-export async function GET(req: Request) {
+export async function POST(req: Request) {
 
   const { data: t } = trpc.tasks.getTasks.useQuery()
   const { data: u } = trpc.users.getUsers.useQuery()
   const { mutate: createReport, isLoading: isCreating } = trpc.reports.createReport.useMutation();
 
-  try {
+  useEffect(() => {
     if (t?.tasks !== undefined) {
       u?.users.map((user) => {
         if (user.role !== 'ADMIN') {
@@ -62,8 +62,6 @@ export async function GET(req: Request) {
         }
       })
     }
+  }, [t])
 
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
 }
