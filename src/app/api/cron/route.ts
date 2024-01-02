d842dcdb988af74e8c411978e27819d02322947c
congtrieu98 <trieunguyen2806@gmail.com>
2024-01-02 13:42:40 +0700

    bbbbbbbbb

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,26 +1,28 @@
 
 import { EmailTemplate } from "@/components/emails/FirstEmail";
+import { createReport } from "@/lib/api/reports/mutations";
+import { Report } from "@/lib/db/schema/reports";
 import { resend } from "@/lib/email/index";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   const email = 'trieunguyen2806@gmail.com'
-  const firstName = 'cong trieu suzu 02/01/200024'
-//   const { report, error } = await createReport({
-//     assignedTo: 'trieuka',
-//     reportDate: new Date(),
-//     jobCompleted: 1,
-//     jobUnfinished: 2,
-//     jobCompletedPrecent:3,
-//     jobUnfinishedPercent: 4,
-//     kpi: 'dat'
-// });
+  const firstName = 'cong trieu suzu tesst dataa'
+  const { report, error } = await createReport({
+    assignedTo: 'trieuka',
+    reportDate: new Date(),
+    jobCompleted: 1,
+    jobUnfinished: 2,
+    jobCompletedPrecent:3,
+    jobUnfinishedPercent: 4,
+    kpi: 'dat'
+});
   try {
     const data = await resend.emails.send({
       from: `SZG <${process.env.RESEND_EMAIL}>`,
       to: [email],
       subject: "Hello world Cron Job!",
-      react: EmailTemplate({ firstName: firstName }),
+      react: EmailTemplate({ firstName: firstName, report: report as Report }),
       text: "Email powered by Resend creon job.",
     });
 
--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,31 +1,31 @@
-import { EmailTemplate } from "@/components/emails/FirstEmail";
-import { resend } from "@/lib/email/index";
-import { emailSchema } from "@/lib/email/utils";
-import { NextResponse } from "next/server";
+// import { EmailTemplate } from "@/components/emails/FirstEmail";
+// import { resend } from "@/lib/email/index";
+// import { emailSchema } from "@/lib/email/utils";
+// import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  const body = await request.json();
-  const { name, email } = emailSchema.parse(body);
-  try {
-  //   const { report, error } = await createReport({
-  //     assignedTo: 'trieuka',
-  //     reportDate: new Date(),
-  //     jobCompleted: 1,
-  //     jobUnfinished: 2,
-  //     jobCompletedPrecent:3,
-  //     jobUnfinishedPercent: 4,
-  //     kpi: 'dat'
-  // });
-    const data = await resend.emails.send({
-      from: `SZG <${process.env.RESEND_EMAIL}>`,
-      to: [email],
-      subject: "Hello world!",
-      react: EmailTemplate({ firstName: name }),
-      text: "Email powered by Resend.",
-    });
+// export async function POST(request: Request) {
+//   const body = await request.json();
+//   const { name, email } = emailSchema.parse(body);
+//   try {
+//   //   const { report, error } = await createReport({
+//   //     assignedTo: 'trieuka',
+//   //     reportDate: new Date(),
+//   //     jobCompleted: 1,
+//   //     jobUnfinished: 2,
+//   //     jobCompletedPrecent:3,
+//   //     jobUnfinishedPercent: 4,
+//   //     kpi: 'dat'
+//   // });
+//     const data = await resend.emails.send({
+//       from: `SZG <${process.env.RESEND_EMAIL}>`,
+//       to: [email],
+//       subject: "Hello world!",
+//       react: EmailTemplate({ firstName: name }),
+//       text: "Email powered by Resend.",
+//     });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
