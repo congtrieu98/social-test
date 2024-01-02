58cb14ce878100c64243a387a30d3774a1e3b749
congtrieu98 <trieunguyen2806@gmail.com>
2024-01-02 17:48:06 +0700

    tytyy

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,25 +1,29 @@
 import { initTRPC } from '@trpc/server';
 import { z } from 'zod';
+import { db } from "@/lib/db/index";
  
-const posts = ['One', 'Two', 'Three'];
+// const posts = ['One', 'Two', 'Three'];
  
-const t = initTRPC.create();
-const router = t.router({
-  post: t.router({
-    add: t.procedure.input(z.string()).mutation((opts) => {
-      posts.push(opts.input);
-      return posts;
-    }),
-  }),
-});
+// const t = initTRPC.create();
+// const router = t.router({
+//   post: t.router({
+//     add: t.procedure.input(z.string()).mutation((opts) => {
+//       posts.push(opts.input);
+//       return posts;
+//     }),
+//   }),
+// });
  
-const caller = router.createCaller({});
-const result =  caller.post.add('Four');
+// const caller = router.createCaller({});
+// const result =  caller.post.add('Four');
+const getTask = async () => {
+  const t = await db.task.findMany({ include: { user: true } })
+  
+  console.log("hjsgfjhsdgfjshfdf", t)
+console.log("hjsgfjhsdgfjshfdf", t)
+console.log("hjsgfjhsdgfjshfdf", t)
+console.log("hjsgfjhsdgfjshfdf", t)
+  return t
+} 
 
-console.log("hjsgfjhsdgfjshfdf", result)
-console.log("hjsgfjhsdgfjshfdf", result)
-console.log("hjsgfjhsdgfjshfdf", result)
-console.log("hjsgfjhsdgfjshfdf", result)
-console.log("hjsgfjhsdgfjshfdf", result)
-console.log("hjsgfjhsdgfjshfdf", result)
-console.log("hjsgfjhsdgfjshfdf", result)
+getTask()
