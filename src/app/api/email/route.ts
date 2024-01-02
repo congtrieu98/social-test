3fa2576db8690534be8be343b7f5d80498b9594f
congtrieu98 <trieunguyen2806@gmail.com>
2024-01-02 13:33:26 +0700

    abc

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -9,20 +9,20 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { name, email } = emailSchema.parse(body);
   try {
-    const { report, error } = await createReport({
-      assignedTo: 'trieuka',
-      reportDate: new Date(),
-      jobCompleted: 1,
-      jobUnfinished: 2,
-      jobCompletedPrecent:3,
-      jobUnfinishedPercent: 4,
-      kpi: 'dat'
-  });
+  //   const { report, error } = await createReport({
+  //     assignedTo: 'trieuka',
+  //     reportDate: new Date(),
+  //     jobCompleted: 1,
+  //     jobUnfinished: 2,
+  //     jobCompletedPrecent:3,
+  //     jobUnfinishedPercent: 4,
+  //     kpi: 'dat'
+  // });
     const data = await resend.emails.send({
       from: `SZG <${process.env.RESEND_EMAIL}>`,
       to: [email],
       subject: "Hello world!",
-      react: EmailTemplate({ firstName: name, report: report as Report }),
+      react: EmailTemplate({ firstName: name }),
       text: "Email powered by Resend.",
     });
 
