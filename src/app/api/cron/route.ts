ffdcf6b8542ccb000c144d38e011520dcafd6f76
congtrieu98 <trieunguyen2806@gmail.com>
2024-01-02 17:04:58 +0700

    vvvv

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,35 +1,64 @@
+import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
 
-import { EmailTemplate } from "@/components/emails/FirstEmail";
-import { createReport } from "@/lib/api/reports/mutations";
-import { Report } from "@/lib/db/schema/reports";
-import { resend } from "@/lib/email/index";
-import { NextResponse } from "next/server";
+type Context = {
+  foo: string;
+};
 
-export async function GET() {
-  const email = 'trieunguyen2806@gmail.com'
-  const firstName = 'cong trieu suzu tesst dataatttt'
-  
-  try {
-    const { report } = await createReport({
-      assignedTo: 'trieukaaaatttt',
-      reportDate: new Date(),
-      jobCompleted: 1,
-      jobUnfinished: 2,
-      jobCompletedPrecent:3,
-      jobUnfinishedPercent: 4,
-      kpi: 'dat'
-  });
-    const data = await resend.emails.send({
-      from: `SZG <${process.env.RESEND_EMAIL}>`,
-      to: [email],
-      subject: "Hello world Cron Job!",
-      react: EmailTemplate({ firstName: firstName, report: report as Report }),
-      text: "Email powered by Resend creon job.",
-    });
+const t = initTRPC.context<Context>().create();
 
-   
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
+const publicProcedure = t.procedure;
+//@ts-ignore
+const { createCallerFactory, router } = t;
+
+interface Post {
+  id: string;
+  title: string;
 }
+const posts: Post[] = [
+  {
+    id: '1',
+    title: 'Hello world',
+  },
+];
+const appRouter = router({
+  post: router({
+    add: publicProcedure
+      .input(
+        z.object({
+          title: z.string().min(2),
+        }),
+      )
+      .mutation((opts) => {
+        const post: Post = {
+          ...opts.input,
+          id: `${Math.random()}`,
+        };
+        posts.push(post);
+        return post;
+      }),
+    list: publicProcedure.query(() => posts),
+  }),
+});
+
+// 1. create a caller-function for your router
+const createCaller = createCallerFactory(appRouter);
+
+// 2. create a caller using your `Context`
+const caller = createCaller({
+  foo: 'bar',
+});
+
+// 3. use the caller to add and list posts
+const addedPost =  caller.post.add({
+  title: 'How to make server-side call in tRPC',
+});
+
+const postList =  caller.post.list();
+console.log("addedPost", addedPost)
+console.log("yueregfgdfgdfbj")
+console.log("yueregfgdfgdfbj")
+console.log("postList:", postList)
+console.log("yueregfgdfgdfbj")
+console.log("yueregfgdfgdfbj")
+console.log("yueregfgdfgdfbj")
