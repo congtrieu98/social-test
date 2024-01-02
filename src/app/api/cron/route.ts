919e242862f93c250fff874d6f906915d0071a3a
congtrieu98 <trieunguyen2806@gmail.com>
2024-01-02 17:36:52 +0700

    ttttt

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,64 +1,25 @@
 import { initTRPC } from '@trpc/server';
 import { z } from 'zod';
-
-type Context = {
-  foo: string;
-};
-
-const t = initTRPC.context<Context>().create();
-
-const publicProcedure = t.procedure;
-//@ts-ignore
-const { createCallerFactory, router } = t;
-
-interface Post {
-  id: string;
-  title: string;
-}
-const posts: Post[] = [
-  {
-    id: '1',
-    title: 'Hello world',
-  },
-];
-const appRouter = router({
-  post: router({
-    add: publicProcedure
-      .input(
-        z.object({
-          title: z.string().min(2),
-        }),
-      )
-      .mutation((opts) => {
-        const post: Post = {
-          ...opts.input,
-          id: `${Math.random()}`,
-        };
-        posts.push(post);
-        return post;
-      }),
-    list: publicProcedure.query(() => posts),
+ 
+const posts = ['One', 'Two', 'Three'];
+ 
+const t = initTRPC.create();
+const router = t.router({
+  post: t.router({
+    add: t.procedure.input(z.string()).mutation((opts) => {
+      posts.push(opts.input);
+      return posts;
+    }),
   }),
 });
+ 
+const caller = router.createCaller({});
+const result =  caller.post.add('Four');
 
-// 1. create a caller-function for your router
-const createCaller = createCallerFactory(appRouter);
-
-// 2. create a caller using your `Context`
-const caller = createCaller({
-  foo: 'bar',
-});
-
-// 3. use the caller to add and list posts
-const addedPost =  caller.post.add({
-  title: 'How to make server-side call in tRPC',
-});
-
-const postList =  caller.post.list();
-console.log("addedPost", addedPost)
-console.log("yueregfgdfgdfbj")
-console.log("yueregfgdfgdfbj")
-console.log("postList:", postList)
-console.log("yueregfgdfgdfbj")
-console.log("yueregfgdfgdfbj")
-console.log("yueregfgdfgdfbj")
+console.log("hjsgfjhsdgfjshfdf", result)
+console.log("hjsgfjhsdgfjshfdf", result)
+console.log("hjsgfjhsdgfjshfdf", result)
+console.log("hjsgfjhsdgfjshfdf", result)
+console.log("hjsgfjhsdgfjshfdf", result)
+console.log("hjsgfjhsdgfjshfdf", result)
+console.log("hjsgfjhsdgfjshfdf", result)
