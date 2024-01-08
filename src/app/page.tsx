6bceb0efe5244e8148b7af49a60e11182e26d09d
congtrieu98 <trieunguyen2806@gmail.com>
2024-01-08 18:02:53 +0700

    custom listTask

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
-'use client'
-import SignIn from '@/components/auth/SignIn';
-import { trpc } from '@/lib/trpc/client';
+"use client";
+import SignIn from "@/components/auth/SignIn";
+import { trpc } from "@/lib/trpc/client";
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UserOutlined,
   VideoCameraOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import { Table } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
-import { formatDateFull, formatDatetime } from '@/utils/constant';
-import moment from 'moment';
-import { Report } from '@/lib/db/schema/reports';
+} from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { Table } from "antd";
+import type { ColumnsType } from "antd/es/table";
+import { formatDateFull, formatDatetime } from "@/utils/constant";
+import moment from "moment";
+import { Report } from "@/lib/db/schema/reports";
 
 const { Header, Sider, Content } = Layout;
 
@@ -23,61 +23,61 @@ export default function Home() {
   const router = useRouter();
   const { data: session } = useSession();
   const [collapsed, setCollapsed] = useState(false);
-  const { data: r } = trpc.reports.getReports.useQuery()
+  const { data: r } = trpc.reports.getReports.useQuery();
 
   const columns: ColumnsType<Report> = [
     {
-      title: 'Ngày',
-      dataIndex: 'reportDate',
-      key: 'reportDate',
-      fixed: 'left',
-      render: (val) => moment(val, formatDateFull).format(formatDatetime)
+      title: "Ngày",
+      dataIndex: "reportDate",
+      key: "reportDate",
+      // fixed: 'left',
+      render: (val) => moment(val, formatDateFull).format(formatDatetime),
     },
     {
-      title: 'Người thực hiện',
-      dataIndex: 'assignedTo',
-      key: 'assignedTo'
+      title: "Người thực hiện",
+      dataIndex: "assignedTo",
+      key: "assignedTo",
     },
     {
-      title: 'Công việc(SL)',
+      title: "Công việc(SL)",
       children: [
         {
-          title: 'Hoàn thành',
-          dataIndex: 'jobCompleted',
-          key: 'jobCompleted',
+          title: "Hoàn thành",
+          dataIndex: "jobCompleted",
+          key: "jobCompleted",
         },
         {
-          title: 'Chưa hoàn thành',
-          dataIndex: 'jobUnfinished',
-          key: 'jobUnfinished',
+          title: "Chưa hoàn thành",
+          dataIndex: "jobUnfinished",
+          key: "jobUnfinished",
         },
       ],
     },
     {
-      title: 'Tỉ lệ(%)',
+      title: "Tỉ lệ(%)",
       children: [
         {
-          title: 'Hoàn thành',
-          dataIndex: 'jobCompletedPrecent',
-          key: 'jobCompletedPrecent',
+          title: "Hoàn thành",
+          dataIndex: "jobCompletedPrecent",
+          key: "jobCompletedPrecent",
           width: 200,
         },
         {
-          title: 'Chưa hoàn thành',
-          dataIndex: 'jobUnfinishedPercent',
-          key: 'jobUnfinishedPercent',
+          title: "Chưa hoàn thành",
+          dataIndex: "jobUnfinishedPercent",
+          key: "jobUnfinishedPercent",
         },
       ],
     },
     {
-      title: 'KPI',
-      dataIndex: 'kpi',
-      key: 'kpi',
-      fixed: 'right',
+      title: "KPI",
+      dataIndex: "kpi",
+      key: "kpi",
+      // fixed: 'right',
     },
     {
-      title: 'Action',
-      fixed: 'right',
+      title: "Action",
+      // fixed: 'right',
     },
   ];
 
@@ -88,18 +88,18 @@ export default function Home() {
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['/']}
+          defaultSelectedKeys={["/"]}
           onClick={(e) => router.push(e.key)}
           items={[
             {
-              key: '/',
+              key: "/",
               icon: <UserOutlined />,
-              label: 'dashboard',
+              label: "dashboard",
             },
             session && {
-              key: 'tasks',
+              key: "tasks",
               icon: <VideoCameraOutlined />,
-              label: 'Danh sách công việc',
+              label: "Danh sách công việc",
             },
           ]}
         />
@@ -112,28 +112,31 @@ export default function Home() {
             padding: 0,
           }}
         >
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            onClick: () => setCollapsed(!collapsed),
-          })}
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
         </Header>
         <Content
           className="site-layout-background"
           style={{
-            margin: '24px 16px',
+            margin: "24px 16px",
             padding: 24,
           }}
         >
-          <div className='text-2xl font-semibold mb-5'>Report</div>
+          <div className="text-2xl font-semibold mb-5">Report</div>
           <Table
             columns={columns}
             dataSource={r?.reports}
             bordered
             size="middle"
-            scroll={{ x: 'calc(700px + 50%)', y: 240 }}
+            scroll={{ x: "calc(700px + 50%)", y: 240 }}
           />
         </Content>
       </Layout>
     </Layout>
-  )
+  );
 }
--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -17,7 +17,16 @@ import { CompleteTask } from "@/lib/db/schema/tasks";
 import { trpc } from "@/lib/trpc/client";
 import TaskModal from "./TaskModal";
 import moment from "moment";
-import { Badge, Layout, Table } from "antd";
+import {
+  Badge,
+  Form,
+  Input,
+  InputNumber,
+  Layout,
+  Popconfirm,
+  Table,
+  Typography,
+} from "antd";
 import type { ColumnsType } from "antd/es/table";
 import { useState } from "react";
 import { formatDateFull, formatDatetime } from "@/utils/constant";
@@ -26,86 +35,22 @@ import { toast } from "../ui/use-toast";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 
-const columns: ColumnsType<CompleteTask> = [
-  {
-    title: "Tên công việc",
-    width: 150,
-    render: (record) => {
-      return <Link href={`/tasks/${record.id}`}>{record.title}</Link>;
-    },
-  },
-  {
-    title: "Người thực hiện",
-    dataIndex: "user",
-    render: (val) => val.name,
-  },
-  {
-    title: "Mức độ ưu tiên",
-    dataIndex: "priority",
-  },
-  {
-    title: "Trạng thái",
-    dataIndex: "status",
-    render: (val) => {
-      if (val === "new") {
-        return "Mới tạo";
-      } else if (val === "readed") {
-        return "Đã xem";
-      } else if (val === "inprogress") {
-        return "Đang thực hiện";
-      } else if (val === "reject") {
-        return "Chưa hoàn thành";
-      } else {
-        return "Đã hoàn thành";
-      }
-    },
-    filters: [
-      {
-        text: "Mới tạo",
-        value: "new",
-      },
-      {
-        text: "Đã xem",
-        value: "readed",
-      },
-      {
-        text: "Đang thực hiện",
-        value: "inprogress",
-      },
-      {
-        text: "Chưa hoàn thành",
-        value: "reject",
-      },
-      {
-        text: "Đã hoàn thành",
-        value: "completed",
-      },
-    ],
-    filterMode: "tree",
-    filterSearch: true,
-    // @ts-ignore
-    onFilter: (value: string, record) => record.status === value,
-    width: "10%",
-  },
-  {
-    title: "Thời gian bắt đầu",
-    dataIndex: "createAt",
-    render: (value) => moment(value, formatDateFull).format(formatDatetime),
-  },
-  {
-    title: "Thời gian kết thúc",
-    dataIndex: "deadlines",
-    render: (value) => moment(value, formatDateFull).format(formatDatetime),
-  },
-  {
-    title: "Action",
-    // dataIndex: 'user',
-    render: (record) => <TaskModal task={record} />,
-  },
-];
+interface EditableCellProps extends React.HTMLAttributes<HTMLElement> {
+  editing: boolean;
+  dataIndex: string;
+  title: any;
+  inputType: "number" | "text";
+  record: CompleteTask;
+  index: number;
+  children: React.ReactNode;
+}
+
 export default function TaskList({ tasks }: { tasks: CompleteTask[] }) {
+  const [form] = Form.useForm();
   const router = useRouter();
   const utils = trpc.useContext();
+  const [editingKey, setEditingKey] = useState("");
+
   const { data: session } = useSession();
   const onSuccess = async (action: "create" | "update" | "delete") => {
     await utils.tasks.getTasks.invalidate();
@@ -119,6 +64,8 @@ export default function TaskList({ tasks }: { tasks: CompleteTask[] }) {
   const { data: t } = trpc.tasks.getTasks.useQuery(undefined, {
     initialData: { tasks },
   });
+
+  const [dataSource, setDataSource] = useState(t.tasks);
 
   const { mutate: deleteTask, isLoading: isDeleting } =
     trpc.tasks.deleteTask.useMutation({
@@ -135,42 +82,183 @@ export default function TaskList({ tasks }: { tasks: CompleteTask[] }) {
     onChange: onSelectChange,
   };
 
-  // const expandedRowRender = (record: CompleteTask) => {
-  //   // console.log(record);
-  //   const data = taskUp?.todoList?.filter(
-  //     (item: TaskUpdate) => item?.taskId === record?.id
-  //   );
-  //   const columnsTaskUp: ColumnsType<CompleteTaskUpdate> = [
-  //     {
-  //       title: "User",
-  //       key: "user",
-  //       render(record) {
-  //         return record?.user?.name;
-  //       },
-  //     },
-  //     {
-  //       title: "Truy cập lúc",
-  //       dataIndex: "updateAt",
-  //       key: "truy cap",
-  //       render: (value) => moment(value, formatDateFull).format(formatDatetime),
-  //     },
-  //     {
-  //       title: "Status",
-  //       key: "state",
-  //       dataIndex: "status",
-  //       render: (val) => (
-  //         <Badge status="success" text={val === "readed" ? "Đã xem" : ""} />
-  //       ),
-  //     },
-  //   ];
-  //   return (
-  //     <Table columns={columnsTaskUp} dataSource={data} pagination={false} />
-  //   );
-  // };
+  const isEditing = (record: CompleteTask) => record.id === editingKey;
 
   if (t.tasks.length === 0) {
     return <EmptyState />;
   }
+
+  const edit = (record: CompleteTask) => {
+    form.setFieldsValue({ name: "", age: "", address: "", ...record });
+    setEditingKey(record.id);
+  };
+
+  const cancel = () => {
+    setEditingKey("");
+  };
+
+  const EditableCell: React.FC<EditableCellProps> = ({
+    editing,
+    dataIndex,
+    title,
+    inputType,
+    record,
+    index,
+    children,
+    ...restProps
+  }) => {
+    const inputNode = inputType === "number" ? <InputNumber /> : <Input />;
+
+    return (
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item
+            name={dataIndex}
+            style={{ margin: 0 }}
+            rules={[
+              {
+                required: true,
+                message: `Vui lòng nhập ${title}!`,
+              },
+            ]}
+          >
+            {inputNode}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  };
+  const columns: ColumnsType<CompleteTask> = [
+    {
+      title: "Tên công việc",
+      width: 150,
+      render: (record) => {
+        return <Link href={`/tasks/${record.id}`}>{record.title}</Link>;
+      },
+    },
+    {
+      title: "Người thực hiện",
+      dataIndex: "user",
+      render: (val) => val.name,
+      //@ts-ignore
+      editable: true,
+    },
+    {
+      title: "Mức độ ưu tiên",
+      dataIndex: "priority",
+      //@ts-ignore
+      editable: true,
+    },
+    {
+      title: "Trạng thái",
+      dataIndex: "status",
+      render: (val) => {
+        if (val === "new") {
+          return "Mới tạo";
+        } else if (val === "readed") {
+          return "Đã xem";
+        } else if (val === "inprogress") {
+          return "Đang thực hiện";
+        } else if (val === "reject") {
+          return "Chưa hoàn thành";
+        } else {
+          return "Đã hoàn thành";
+        }
+      },
+      filters: [
+        {
+          text: "Mới tạo",
+          value: "new",
+        },
+        {
+          text: "Đã xem",
+          value: "readed",
+        },
+        {
+          text: "Đang thực hiện",
+          value: "inprogress",
+        },
+        {
+          text: "Chưa hoàn thành",
+          value: "reject",
+        },
+        {
+          text: "Đã hoàn thành",
+          value: "completed",
+        },
+      ],
+      filterMode: "tree",
+      filterSearch: true,
+      // @ts-ignore
+      onFilter: (value: string, record) => record.status === value,
+      width: "10%",
+      //@ts-ignore
+      editable: true,
+    },
+    {
+      title: "Thời gian bắt đầu",
+      dataIndex: "createAt",
+      render: (value) => moment(value, formatDateFull).format(formatDatetime),
+    },
+    {
+      title: "Thời gian kết thúc",
+      dataIndex: "deadlines",
+      render: (value) => moment(value, formatDateFull).format(formatDatetime),
+    },
+    {
+      title: "operation",
+      dataIndex: "operation",
+      render: (_: any, record: CompleteTask) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <Typography.Link
+              // onClick={() => save(record.id)}
+              style={{ marginRight: 8 }}
+            >
+              Save
+            </Typography.Link>
+            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <Typography.Link
+            disabled={editingKey !== ""}
+            onClick={() => edit(record)}
+          >
+            Edit
+          </Typography.Link>
+        );
+      },
+    },
+    {
+      title: "Action",
+      // dataIndex: 'user',
+      render: (record) => <TaskModal task={record} />,
+    },
+  ];
+
+  const mergedColumns = columns.map((col) => {
+    //@ts-ignore
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record: CompleteTask) => ({
+        record,
+        //@ts-ignore
+        inputType: col.dataIndex === "age" ? "number" : "text",
+        //@ts-ignore
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
 
   return (
     <div className="relative">
@@ -204,17 +292,26 @@ export default function TaskList({ tasks }: { tasks: CompleteTask[] }) {
       )}
       <Layout className="layoutContent">
         <Layout.Content>
-          <Table
-            rowKey="id"
-            loading={isDeleting}
-            // @ts-ignore
-            rowSelection={session?.user.role === "ADMIN" && rowSelection}
-            columns={columns}
-            // @ts-ignore
-            // expandable={session?.user.role === "ADMIN" && { expandedRowRender }}
-            dataSource={t.tasks}
-            scroll={{ x: 1200 }}
-          />
+          <Form form={form} component={false}>
+            <Table
+              rowKey="id"
+              loading={isDeleting}
+              // @ts-ignore
+              rowSelection={session?.user.role === "ADMIN" && rowSelection}
+              components={{
+                body: {
+                  cell: EditableCell,
+                },
+              }}
+              //@ts-ignore
+              columns={mergedColumns}
+              // @ts-ignore
+              // expandable={session?.user.role === "ADMIN" && { expandedRowRender }}
+              dataSource={dataSource}
+              rowClassName="editable-row"
+              scroll={{ x: 1200 }}
+            />
+          </Form>
         </Layout.Content>
       </Layout>
     </div>
