96d84e0361afdcae80c55577ea955c072b025b03
congtrieu98 <trieunguyen2806@gmail.com>
2024-01-18 17:24:14 +0700

    bbbbnnnmmm

--- src/components/general/form/dateForm.tsx
+++ src/components/general/form/dateForm.tsx
@@ -82,15 +82,16 @@ const DateForm = ({
                     variant={"outline"}
                     className={cn(
                       "w-[240px] pl-3 text-left font-normal",
-                      !field.value && "text-muted-foreground"
+                      !(field.value ? field.value : date) && "text-muted-foreground"
                     )}
                     disabled={session?.user?.role !== ROLE.ADMIN}
                   >
-                    {field.value ? (
-                      format(new Date(field.value), "PPP")
-                    ) : (
-                      <span>Pick a date</span>
-                    )}
+                    {date ? (
+                      format(date, "PPP")
+                    ) :
+                      (
+                        <span>Pick a date</span>
+                      )}
                     <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                   </Button>
                 </FormControl>
@@ -98,16 +99,17 @@ const DateForm = ({
               <PopoverContent className="w-auto p-0" align="start">
                 <Calendar
                   mode="single"
-                  selected={field.value}
+                  selected={date}
                   onSelect={setDate}
+                  // onSelect={field.onChange}
                   disabled={(date) => name === "deadlines" && date < new Date()
                     // : date > new Date() || date < new Date("1900-01-01")
                   }
                   initialFocus
                 />
-                <div className="p-3 border-t border-border">
+                {/* <div className="p-3 border-t border-border">
                   <TimePickerDemo setDate={setDate} date={date} />
-                </div>
+                </div> */}
               </PopoverContent>
             </Popover>
             {/* <DateTimePicker
