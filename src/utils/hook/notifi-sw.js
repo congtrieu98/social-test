19df2e6941bebf6fbdb2c64ac8a3989748b9fe96
congtrieu98 <trieunguyen2806@gmail.com>
2024-02-23 17:36:52 +0700

    ksdfhksdfjkfsd

---
+++ src/utils/hook/notifi-sw.js
@@ -0,0 +1,15 @@
+// export function noti() {
+//   const checkpermission = () => {
+//     if (!("serviceWorker" in navigator)) {
+//       throw new Error("No support service worker");
+//     }
+//   };
+
+//   const registerSW = async () => {
+//     const registration = await navigator.serviceWorker.register("sw.js");
+//     return registration;
+//   };
+
+//   checkpermission();
+//   registerSW();
+// }
